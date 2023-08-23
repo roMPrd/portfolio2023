ced7423fae23c73f00b57deeaad9534082011d49
Romain DELIMAL <rom.delimal@gmail.com>
2023-08-23 22:11:42 +0200

    blur effect

--- app/page.jsx
+++ app/page.jsx
@@ -4,10 +4,12 @@ import Navbar from '@components/sections/Navbar'
 import About from '@components/sections/About'
 import Projects from '@components/sections/Projects'
 import Contact from '@components/sections/Contact'
+import Blur from '@components/utilities/Blur'
 
 const Home = () => {
   return (
     <div className="flex flex-col items-center justify-center bg-black">
+      <Blur />
       <Navbar />
       <HomeStart />
       <HomePage />
