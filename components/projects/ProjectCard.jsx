dd14e1e89bc74fd7668c2727b9337091379098e6
Romain DELIMAL <rom.delimal@gmail.com>
2023-08-25 16:39:22 +0200

    tweeked projectCard

--- components/projects/ProjectCard.jsx
+++ components/projects/ProjectCard.jsx
@@ -22,20 +22,20 @@ const ProjectCard = ({
                   href={`${github}`}
                   target="_blank"
                   aria-label="Open GitHub Repository"
-                  className="h-20 w-20 mr-5 mb-5 text-black bg-[#D2D4D5] border-10 border-[#ffffff3d] rounded-full flex justify-center items-center text-center">
-                    <i><SiGithub className="h-10 w-10" /></i>
+                  className="h-[70px] w-[70px] mr-5 mb-2 text-black bg-[#D2D4D5] border-10 border-[#ffffff3d] rounded-full flex justify-center items-center text-center">
+                    <i><SiGithub className="h-[35px] w-[35px]" /></i>
                 </a>
                 <a
                   rel="stylesheet"
                   href={`${website}`}
                   target="_blank"
                   aria-label="Open Web Page"
-                  className="h-20 w-20 mr-5 mb-5 text-black bg-[#D2D4D5] border-10 border-[#ffffff4d] rounded-full flex justify-center items-center text-center">
-                    <i><BsLink45Deg className="h-10 w-10" /></i>
+                  className="h-[70px] w-[70px] mr-5 mb-2 text-black bg-[#D2D4D5] border-10 border-[#ffffff4d] rounded-full flex justify-center items-center text-center">
+                    <i><BsLink45Deg className="h-[35px] w-[35px]" /></i>
                 </a>
               </div>
-              <p className='mb-5 text-[40px]'>{name}</p>
-              <p className='mb-5 text-[16px] font-semibold'>{description}</p>
+              <p className='mb-5 text-[40px] font-bold'>{name}</p>
+              <p className='mb-5 text-[16px] font-semibold w-[90%]'>{description}</p>
               <p className='mb-5'>{stacks}</p>
             </div>
             <div className="w-[50%] p-9 flex flex-col justify-center items-center">
