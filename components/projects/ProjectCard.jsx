3f6e9c117312ccbf3af154b1ae5d744a3e792036
Romain DELIMAL <rom.delimal@gmail.com>
2023-08-27 16:00:21 +0200

    cards img css

--- components/projects/ProjectCard.jsx
+++ components/projects/ProjectCard.jsx
@@ -19,10 +19,10 @@ const ProjectCard = ({
       // console.log(id),
       <div className="relative h-[550px] mb-4">
         <div style={{ background: "linear-gradient(0deg, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 0.1) 100%)" }}
-          className={`relative w-full h-full border-2 rounded-[25px] border-[#ffffff3d]
-            ${id % 2 === 0 ? 'flex flex-row row-2' : 'flex flex-row-reverse row-2'}`}>
-            <div className="w-[50%] p-9 flex flex-col ">
-              <div className={`flex justify-start items-center
+          className={`relative flex w-full h-full border-2 rounded-[25px] border-[#ffffff3d]
+            ${id % 2 === 0 ? 'justify-start' : 'justify-end text-right'}`}>
+            <div className=" p-6 flex flex-col mt-4">
+              <div className={`flex items-center
                 ${id % 2 === 0 ? 'justify-start' : 'justify-end'}`}>
               {github ? (
                   <Link
@@ -30,7 +30,8 @@ const ProjectCard = ({
                     href={`${github}`}
                     target="_blank"
                     aria-label="Open GitHub Repository"
-                    className="h-[70px] w-[70px] mr-5 mb-20 text-black bg-[#D2D4D5] border-10 border-[#ffffff3d] rounded-full flex justify-center items-center text-center">
+                    className={`h-[70px] w-[70px] mx-4 text-black bg-[#D2D4D5] border-10 border-[#ffffff3d] rounded-full flex justify-center items-center text-center
+                    ${id % 2 === 0 ? '' : ''}`}>
                       <i><SiGithub className="h-[35px] w-[35px]" /></i>
                   </Link>
                 ) : (
@@ -43,7 +44,8 @@ const ProjectCard = ({
                     href={`${website}`}
                     target="_blank"
                     aria-label="Open Web Page"
-                    className="h-[70px] w-[70px] mr-5 mb-20 text-black bg-[#D2D4D5] border-10 border-[#ffffff4d] rounded-full flex justify-center items-center text-center">
+                    className={`h-[70px] w-[70px] mx-4 text-black bg-[#D2D4D5] border-10 border-[#ffffff4d] rounded-full flex justify-center items-center text-center
+                    ${id % 2 === 0 ? '' : ''}`}>
                       <i><BsLink45Deg className="h-[35px] w-[35px]" /></i>
                   </Link>
                 ) :(
@@ -55,25 +57,37 @@ const ProjectCard = ({
 
                 <AnimatedTitles
                   text={name}
-                  className={"mb-5 text-[40px] font-bold"}
+                  className={`mb-5 text-[40px] font-bold ${id % 2 === 0 ? 'mx-4' : ''}`}
                   wordSpace={"mr-[1rem]"}
-                  charSpace={"mr-[0.001em]"}
+                  charSpace={"mr-[0.001em]"
+                }
                 />
                 {/* <p className='mb-5 text-[40px] font-bold'>{name}</p> */}
                 <AnimatedTexts
                   text={description}
-                  className={"mb-5 text-[16px] font-semibold w-[90%]"}
+                  className={"mb-5 mx-4 text-[16px] font-semibold"}
                   delay={0}
                 />
                 {/* // <p className='mb-5 text-[16px] font-semibold w-[90%]'>{description}</p> */}
-                <p className='mb-5 mr-4'>{stacks}</p>
+                <p className='mb-5 mx-4'>{stacks}</p>
               </div>
             </div>
-            <div className="w-[50%] pr-4 flex flex-col justify-center items-center">
+            {/* <div className="w-[50%] pr-4 flex flex-col justify-center items-center">
               <img className='w-full opacity-50 border-2 rounded-[25px] border-[#ffffff3d]' src={`${image}`}></img>
-            </div>
+            </div> */}
         </div>
-        <div className="z-10 absolute inset-0 w-full h-full pointer-events-none bg-gradient-to-t from-black to-transparent to-20%"/>
+        <div>
+          <img
+            src={image}
+            alt={name}
+            width={500}
+            height={500}
+            className={`absolute bottom-[2px] w-[70%] sm:w-[85%] md:w-[60%] lg:max-w-[55%] opacity-80 rounded-[25px] ${
+                id % 2 === 0 ? "right-[2px] boxShadowTL" : "left-[2px] boxShadowTR"
+            }`}
+          />
+        </div>
+        {/* <div className="z-10 absolute inset-0 w-full h-full pointer-events-none bg-gradient-to-t from-black to-transparent to-20%"/> */}
         <div className="grain">
           <Grain baseFrequency={"9"} numOctaves={5} w={"100%"} h={"100%"}/>
         </div>
