65e5cec46fc2718a628bc88a2cc587affa29279f
Romain DELIMAL <rom.delimal@gmail.com>
2023-09-02 16:13:24 +0200

    Updated cover video test 4

--- app/sections/HomePage.jsx
+++ app/sections/HomePage.jsx
@@ -2,20 +2,20 @@ import AnimatedLogo from "@components/animations/animatedLogo"
 
 const HomePage = () => {
   return (
-    <div id="home" className="sectionScroll h-[100vh] w-[100%] flex justify-center items-center relative pointer-events-none">
-      <div className="absolute inset-0 pointer-events-none">
+    <div id="home" className="sectionScroll h-[100vh] w-[100%] flex justify-center items-center relative">
+      <div className="absolute inset-0">
         <video
           autoPlay="autoplay"
           muted="muted"
           loop="loop"
-          className="object-cover h-full w-full pointer-events-none"
+          className="object-cover h-full w-full"
           src="network_1080p.mp4"
           preload="auto">
         </video>
-        <div className="z-5 absolute inset-0 w-full h-full bg-gradient-to-t from-black via-transparent pointer-events-none"/>
+        <div className="z-5 absolute inset-0 w-full h-full bg-gradient-to-t from-black via-transparent"/>
       </div>
       <AnimatedLogo
-        className={"z-10 h-20 absolute pointer-events-none"}
+        className={"z-10 h-20 absolute"}
         src={"Logo_RP_transparent.png"}
         alt={"logo"}
       />
